2dc2ce5a023aba3a7258d1c63a8e86f75dd1d8f5
John Rhe Poblacion <johnrhe.poblacion@gmail.com>
2025-03-29 16:00:23 +0800

    Allowed Host

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    allowedHosts: [
+      'portfolio-final-1-zy5t.onrender.com',
+      '.onrender.com'
+    ]
+  }
 }) 
