cfe01b4e4bd98b4fdbac2ebc819c8aad7d0b1bc3
John Rhe Poblacion <johnrhe.poblacion@gmail.com>
2025-03-29 15:43:37 +0800

    Deploy2.0

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 10000,
+    allowedHosts: [
+      'portfolio-final-3rcq.onrender.com',
+      '.onrender.com'
+    ]
+  }
 }) 
